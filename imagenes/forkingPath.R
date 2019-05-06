31c8cdd554bf6a28482a9f4386d2ab73e5f40478
Gustavo Landfried <gustavolandfried@gmail.com>
2019-05-06 16:49:41 -0300

    forking path, step by step

--- imagenes/forkingPath.R
+++ imagenes/forkingPath.R
@@ -79,6 +79,7 @@ nodes <- function(patron=c(19,1,19),creencia=c(1,1,1,19), base1,base2,base3){
   
 }
 
+
 forkingPath <- function(creencia,path=T){
   plot(0,0,col=rgb(0,0,0,0),axes=F,ylab="",xlab="",ylim=c(-3,3),xlim=c(-3,3))
   
@@ -119,6 +120,106 @@ forkingPath(c(1,1,1,19))
 forkingPath(c(1,1,19,19))
 forkingPath(c(1,19,19,19))
 forkingPath(c(19,19,19,19))
+
+creencia = c("4","3","2","1")
+
+plot(0,0,col=rgb(0,0,0,0),axes=F,ylab="",xlab="",ylim=c(-3,3),xlim=c(-3,3))
+
+base1 <- seq(0,pi,length.out = 4+2)[c(-1,-6)]
+base2 <- seq(0,pi,length.out = 4*4+2)[c(-1,-18)]
+base3 <- seq(0,pi,length.out = 4*4*4+2)[c(-1,-(16*4+2))]
+
+puntos(base3,3,pch=creencia,col=rgb(0,0,0,0.01))
+
+for (a in seq(4)){#a=1
+  segmento(c(0),base1[a],0,rgb(0,0,0,1))
+}
+
+puntos(c(0),0,pch=19,col=rgb(1,1,1,1),2)
+puntos(base1,1,pch=19,col=rgb(1,1,1,1),2)
+puntos(base1,1,pch=creencia,col=rgb(0,0,0,1))
+
+
+plot(0,0,col=rgb(0,0,0,0),axes=F,ylab="",xlab="",ylim=c(-3,3),xlim=c(-3,3))
+
+base1 <- seq(0,pi,length.out = 4+2)[c(-1,-6)]
+base2 <- seq(0,pi,length.out = 4*4+2)[c(-1,-18)]
+base3 <- seq(0,pi,length.out = 4*4*4+2)[c(-1,-(16*4+2))]
+
+puntos(base3,3,pch=creencia,col=rgb(0,0,0,0.01))
+
+for (a in seq(4)){#a=1
+  segmento(c(0),base1[a],0,rgb(0,0,0,1))
+  if (a == 1){
+    for (b in seq(4)){#b=1
+      segmento(base1[a],base2[4*(a-1)+b],1,rgb(0,0,0,1))
+    }  
+  }
+}
+
+puntos(c(0),0,pch=19,col=rgb(1,1,1,1),2)
+puntos(base1,1,pch=19,col=rgb(1,1,1,1),2)
+puntos(base1,1,pch=creencia,col=rgb(0,0,0,1))
+puntos(base2,2,pch=19,col=rgb(1,1,1,1),2)
+puntos(base2[1:4],2,pch=creencia,col=rgb(0,0,0,1))
+
+
+plot(0,0,col=rgb(0,0,0,0),axes=F,ylab="",xlab="",ylim=c(-3,3),xlim=c(-3,3))
+
+base1 <- seq(0,pi,length.out = 4+2)[c(-1,-6)]
+base2 <- seq(0,pi,length.out = 4*4+2)[c(-1,-18)]
+base3 <- seq(0,pi,length.out = 4*4*4+2)[c(-1,-(16*4+2))]
+
+puntos(base3,3,pch=creencia,col=rgb(0,0,0,0.01))
+
+for (a in seq(4)){#a=1
+  segmento(c(0),base1[a],0,rgb(0,0,0,1))
+  
+    for (b in seq(4)){#b=1
+      segmento(base1[a],base2[4*(a-1)+b],1,rgb(0,0,0,1))
+    }  
+  
+}
+
+puntos(c(0),0,pch=19,col=rgb(1,1,1,1),2)
+puntos(base1,1,pch=19,col=rgb(1,1,1,1),2)
+puntos(base1,1,pch=creencia,col=rgb(0,0,0,1))
+puntos(base2,2,pch=19,col=rgb(1,1,1,1),2)
+puntos(base2,2,pch=creencia,col=rgb(0,0,0,1))
+
+
+plot(0,0,col=rgb(0,0,0,0),axes=F,ylab="",xlab="",ylim=c(-3,3),xlim=c(-3,3))
+
+base1 <- seq(0,pi,length.out = 4+2)[c(-1,-6)]
+base2 <- seq(0,pi,length.out = 4*4+2)[c(-1,-18)]
+base3 <- seq(0,pi,length.out = 4*4*4+2)[c(-1,-(16*4+2))]
+
+puntos(base3,3,pch=creencia,col=rgb(0,0,0,0.01))
+
+
+for (a in seq(4)){#a=1
+  segmento(c(0),base1[a],0,rgb(0,0,0,1))
+  for (b in seq(4)){#b=1
+    segmento(base1[a],base2[4*(a-1)+b],1,rgb(0,0,0,1))
+    if (b == 1 & a == 1) {
+      for (c in seq(4)){#b=1
+        segmento(base2[4*(a-1)+b],base3[4*((4*(a-1)+b)-1)+c],2,rgb(0,0,0,1))
+      }  
+    }
+  }  
+  
+}
+
+puntos(c(0),0,pch=19,col=rgb(1,1,1,1),2)
+puntos(base1,1,pch=19,col=rgb(1,1,1,1),2)
+puntos(base1,1,pch=creencia,col=rgb(0,0,0,1))
+puntos(base2,2,pch=19,col=rgb(1,1,1,1),2)
+puntos(base2,2,pch=creencia,col=rgb(0,0,0,1))
+puntos(base3,3,pch=19,col=rgb(1,1,1,1),2)
+puntos(base3[1:4],3,pch=creencia,col=rgb(0,0,0,1))
+puntos(base2,2,pch=creencia,col=rgb(0,0,0,1))
+
+forkingPath(c("4","3","2","1"),path=F)
 forkingPath(c(1,1,1,1),path=F)
 
 ######
